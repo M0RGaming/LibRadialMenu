a6233647b136089043f5d371e7f4894442bddf7d
M0RGaming <chinmaytlc@gmail.com>
2025-11-10 13:56:09 -0700

    changed translation for ru quickslot wheel

--- lang/ru.lua
+++ lang/ru.lua
@@ -1,6 +1,6 @@
 local LIBRADIAL_WHEEL = HOTBAR_CATEGORY_MAX_VALUE + 100
 local libradialwheelcategory = string.format("SI_HOTBARCATEGORY%d",LIBRADIAL_WHEEL)
-SafeAddString(_G[libradialwheelcategory], "Аддон Ячейки на колесе быстрого доступа", 1)
+SafeAddString(_G[libradialwheelcategory], "Ячейки быстрого доступа", 1) --Аддон Ячейки на колесе быстрого доступа
 
 SafeAddString(SI_LIBRADIALMENU_ASSIGN_TITLE, "Назначьте действие для слота %d.", 1)
 SafeAddString(SI_LIBRADIALMENU_ASSIGN_SLOT, "Слот %d: ", 1)
