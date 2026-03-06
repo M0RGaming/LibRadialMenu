1fb265803b8cd4a954774cd4fb35b4bac47574fa
M0RGaming <chinmaytlc@gmail.com>
2026-03-06 16:27:07 -0700

    temp console fix

--- LibRadialMenu.lua
+++ LibRadialMenu.lua
@@ -252,7 +252,7 @@ ESO_Dialogs["LibProfileAssignDialogue"] = {
 						}
 					end
 					ZO_Dialogs_ReleaseDialogOnButtonPress("LibProfileAssignDialogue")
-					if LibHarvensAddonSettings.list then
+					if LibHarvensAddonSettings and LibHarvensAddonSettings.list then
 						LibHarvensAddonSettings.list:RefreshVisible()
 					end
 				end
@@ -312,179 +312,181 @@ ESO_Dialogs["LibProfileAssignDialogue"] = {
 	
 	
 ]]
-
-
-
-LibRadialMenu.settings = LibHarvensAddonSettings:AddAddon("Lib Radial Menu")
-local settings = LibRadialMenu.settings
-
-
-
-local getLabel = function(entryData, index)
-	local entryAddon = registeredEntries[entryData.addon]
-	if entryAddon and entryAddon[entryData.entry] and addonNames[entryData.addon] then
-		local entry = entryAddon[entryData.entry]
-		local addonName = addonNames[entryData.addon]
-		local slotIcon = entry.icon or ""
-		local slotname = entry.name or entryData.entry
-		local description = entry.description
-		return string.format("%s|t27:27:%s|t %s", string.format(GetString(SI_LIBRADIALMENU_ASSIGN_SLOT), index), slotIcon, slotname)
-	else
-		return string.format(GetString(SI_LIBRADIALMENU_ASSIGN_SLOT), index)
-	end
-end
-
-local getTooltip = function(entryData)
-	local entryAddon = registeredEntries[entryData.addon]
-	if entryAddon and entryAddon[entryData.entry] and addonNames[entryData.addon] then
-		local entry = entryAddon[entryData.entry]
-		local addonName = addonNames[entryData.addon]
-		local slotIcon = entry.icon or ""
-		local slotname = entry.name or entryData.entry
-		local description = entry.description
-		return string.format("%s\n\n|t27:27:%s|t %s\n\n%s", addonName, slotIcon, slotname, description)
-	else
-		return GetString(SI_LIBRADIALMENU_ASSIGN_NOTHING)
-	end
-end
-
-
-function LibRadialMenu.openSettings()
-	if (LibHarvensAddonSettings.initialized ~= true) and (LibHarvensAddonSettings.scrollList == nil)  then
-		LibHarvensAddonSettings:Initialize()
-	end
-	settings:Select()
-	local headerData = {}
-	headerData.titleText = settings.name
-	headerData.subtitleText = settings.version
-	headerData.messageText = zo_strformat(GetString(SI_ADD_ON_AUTHOR_LINE), "@M0R_Gaming")
-	ZO_GamepadGenericHeader_RefreshData(LibHarvensAddonSettings.scrollList.header, headerData)
-	SCENE_MANAGER:Push("LibHarvensAddonSettingsScene")
-end
-
-
-
-
 LibRadialMenu:RegisterAddon("libradialmenu", "LibRadialMenu")
-LibRadialMenu:RegisterEntry("libradialmenu", GetString(SI_LIBRADIALMENU_OPEN_SETTINGS), "opensettings", "esoui/art/skillsadvisor/advisor_tabicon_settings_up.dds",
-	LibRadialMenu.openSettings,
-	GetString(SI_LIBRADIALMENU_OPEN_SETTINGS_TOOLTIP))
+LibRadialMenu:RegisterEntry("libradialmenu", GetString(SI_ADDON_MANAGER_RELOAD), "reloadui", "/esoui/art/login/gamepad/loading-ouroboros.dds",
+	function() ReloadUI() end,
+	GetString(SI_GAMEPAD_ADDON_MANAGER_DELETE_SAVED_VARIABLES_RELOAD_UI_WARNING))
+
+if LibHarvensAddonSettings then
+	LibRadialMenu.settings = LibHarvensAddonSettings:AddAddon("Lib Radial Menu")
+	local settings = LibRadialMenu.settings
 
 
-function LibRadialMenu.UpdateSettingsMenu()
-	settings:Clear()
 
-	if LibRadialMenu.isCN then
-		if LibRadialMenu.vars.cntype == "tradcn" then
-			LibRadialMenu.loadTradCN()
+	local getLabel = function(entryData, index)
+		local entryAddon = registeredEntries[entryData.addon]
+		if entryAddon and entryAddon[entryData.entry] and addonNames[entryData.addon] then
+			local entry = entryAddon[entryData.entry]
+			local addonName = addonNames[entryData.addon]
+			local slotIcon = entry.icon or ""
+			local slotname = entry.name or entryData.entry
+			local description = entry.description
+			return string.format("%s|t27:27:%s|t %s", string.format(GetString(SI_LIBRADIALMENU_ASSIGN_SLOT), index), slotIcon, slotname)
 		else
-			LibRadialMenu.loadSimpleCN()
-		end
-		LibRadialMenu:RegisterEntry("libradialmenu", GetString(SI_LIBRADIALMENU_OPEN_SETTINGS), "opensettings", "esoui/art/skillsadvisor/advisor_tabicon_settings_up.dds",
-			LibRadialMenu.openSettings,
-			GetString(SI_LIBRADIALMENU_OPEN_SETTINGS_TOOLTIP))
-	end
-
-
-
-
-	local settingsTable = {
-		{
-			type = LibHarvensAddonSettings.ST_SLIDER,
-			label = GetString(SI_LIBRADIALMENU_NUM_SLOTS),
-			tooltip = GetString(SI_LIBRADIALMENU_NUM_SLOTS_TOOLTIP),
-			setFunction = function(value)
-				LibRadialMenu.vars.numSlots = value
-			end,
-			getFunction = function()
-				return LibRadialMenu.vars.numSlots
-			end,
-			default = 12,
-			min = 2,
-			max = 25,
-			step = 1,
-		},
-		{
-			type = LibHarvensAddonSettings.ST_BUTTON,
-			label = GetString(SI_LIBRADIALMENU_REFRESH_MENU),
-			tooltip = GetString(SI_LIBRADIALMENU_REFRESH_MENU_TOOLTIP),
-			clickHandler = LibRadialMenu.UpdateSettingsMenu,
-		},
-		{
-			type = LibHarvensAddonSettings.ST_SECTION,
-			label = GetString(SI_LIBRADIALMENU_ASSIGN_SLOTS_HEADER),
-		},
-	}
-
-
-	for i,v in pairs(LibRadialMenu.vars.slots) do
-		if i > LibRadialMenu.vars.numSlots then
-			LibRadialMenu.vars.slots[i] = nil
+			return string.format(GetString(SI_LIBRADIALMENU_ASSIGN_SLOT), index)
 		end
 	end
 
-	for i=1,LibRadialMenu.vars.numSlots do
-		if (type(LibRadialMenu.vars.slots[i]) ~= "table") then
-			LibRadialMenu.vars.slots[i] = {}
+	local getTooltip = function(entryData)
+		local entryAddon = registeredEntries[entryData.addon]
+		if entryAddon and entryAddon[entryData.entry] and addonNames[entryData.addon] then
+			local entry = entryAddon[entryData.entry]
+			local addonName = addonNames[entryData.addon]
+			local slotIcon = entry.icon or ""
+			local slotname = entry.name or entryData.entry
+			local description = entry.description
+			return string.format("%s\n\n|t27:27:%s|t %s\n\n%s", addonName, slotIcon, slotname, description)
+		else
+			return GetString(SI_LIBRADIALMENU_ASSIGN_NOTHING)
 		end
 	end
 
-	libRadialWheelEntries = LibRadialMenu.vars.slots
 
-	for i=1,#libRadialWheelEntries do
-		settingsTable[#settingsTable+1] = {
-			type = LibHarvensAddonSettings.ST_BUTTON,
-			label = function()
-				return getLabel(libRadialWheelEntries[i], i)
-			end,
-			buttonText = GetString(SI_GAMEPAD_ITEM_ACTION_QUICKSLOT_ASSIGN), 
-			tooltip = function()
-				return getTooltip(libRadialWheelEntries[i])
-			end,
-			clickHandler = function() ZO_Dialogs_ShowPlatformDialog("LibProfileAssignDialogue", {slotIndex=i}) end,
-		}
+	function LibRadialMenu.openSettings()
+		if (LibHarvensAddonSettings.initialized ~= true) and (LibHarvensAddonSettings.scrollList == nil)  then
+			LibHarvensAddonSettings:Initialize()
+		end
+		settings:Select()
+		local headerData = {}
+		headerData.titleText = settings.name
+		headerData.subtitleText = settings.version
+		headerData.messageText = zo_strformat(GetString(SI_ADD_ON_AUTHOR_LINE), "@M0R_Gaming")
+		ZO_GamepadGenericHeader_RefreshData(LibHarvensAddonSettings.scrollList.header, headerData)
+		SCENE_MANAGER:Push("LibHarvensAddonSettingsScene")
 	end
 
-	if LibRadialMenu.isCN then
-		local lookup = {
-			[GetString(SI_LIBRADIALMENU_TRADCN)] = "tradcn",
-			[GetString(SI_LIBRADIALMENU_SIMPLECN)] = "simplecn",
-			["tradcn"] = GetString(SI_LIBRADIALMENU_TRADCN),
-			["simplecn"] = GetString(SI_LIBRADIALMENU_SIMPLECN),
-		}
 
-		table.insert(settingsTable, 1, {
-			type = LibHarvensAddonSettings.ST_DROPDOWN,
-			items = {
-				{name = GetString(SI_LIBRADIALMENU_SIMPLECN), data = "simplecn"},
-				{name = GetString(SI_LIBRADIALMENU_TRADCN), data = "tradcn"},
+
+
+	LibRadialMenu:RegisterEntry("libradialmenu", GetString(SI_LIBRADIALMENU_OPEN_SETTINGS), "opensettings", "esoui/art/skillsadvisor/advisor_tabicon_settings_up.dds",
+		LibRadialMenu.openSettings,
+		GetString(SI_LIBRADIALMENU_OPEN_SETTINGS_TOOLTIP))
+
+
+	function LibRadialMenu.UpdateSettingsMenu()
+		settings:Clear()
+
+		if LibRadialMenu.isCN then
+			if LibRadialMenu.vars.cntype == "tradcn" then
+				LibRadialMenu.loadTradCN()
+			else
+				LibRadialMenu.loadSimpleCN()
+			end
+			LibRadialMenu:RegisterEntry("libradialmenu", GetString(SI_LIBRADIALMENU_OPEN_SETTINGS), "opensettings", "esoui/art/skillsadvisor/advisor_tabicon_settings_up.dds",
+				LibRadialMenu.openSettings,
+				GetString(SI_LIBRADIALMENU_OPEN_SETTINGS_TOOLTIP))
+		end
+
+
+
+
+		local settingsTable = {
+			{
+				type = LibHarvensAddonSettings.ST_SLIDER,
+				label = GetString(SI_LIBRADIALMENU_NUM_SLOTS),
+				tooltip = GetString(SI_LIBRADIALMENU_NUM_SLOTS_TOOLTIP),
+				setFunction = function(value)
+					LibRadialMenu.vars.numSlots = value
+				end,
+				getFunction = function()
+					return LibRadialMenu.vars.numSlots
+				end,
+				default = 12,
+				min = 2,
+				max = 25,
+				step = 1,
 			},
-			label = GetString(SI_LIBRADIALMENU_TRANSLATEDBY),
-			getFunction = function()
-				return lookup[LibRadialMenu.vars.cntype] or GetString(SI_LIBRADIALMENU_SIMPLECN)
-			end,
-			setFunction = function(control, itemName, itemData)
-				LibRadialMenu.vars.cntype = itemData.data or "simplecn"
-				LibRadialMenu.UpdateSettingsMenu()
-			end,
-		})
-	elseif GetString(SI_LIBRADIALMENU_TRANSLATEDBY) ~= "" then
-		table.insert(settingsTable, 1, {
-			type = LibHarvensAddonSettings.ST_SECTION,
-			label = GetString(SI_LIBRADIALMENU_TRANSLATEDBY),
-		})
+			{
+				type = LibHarvensAddonSettings.ST_BUTTON,
+				label = GetString(SI_LIBRADIALMENU_REFRESH_MENU),
+				tooltip = GetString(SI_LIBRADIALMENU_REFRESH_MENU_TOOLTIP),
+				clickHandler = LibRadialMenu.UpdateSettingsMenu,
+			},
+			{
+				type = LibHarvensAddonSettings.ST_SECTION,
+				label = GetString(SI_LIBRADIALMENU_ASSIGN_SLOTS_HEADER),
+			},
+		}
+
+
+		for i,v in pairs(LibRadialMenu.vars.slots) do
+			if i > LibRadialMenu.vars.numSlots then
+				LibRadialMenu.vars.slots[i] = nil
+			end
+		end
+
+		for i=1,LibRadialMenu.vars.numSlots do
+			if (type(LibRadialMenu.vars.slots[i]) ~= "table") then
+				LibRadialMenu.vars.slots[i] = {}
+			end
+		end
+
+		libRadialWheelEntries = LibRadialMenu.vars.slots
+
+		for i=1,#libRadialWheelEntries do
+			settingsTable[#settingsTable+1] = {
+				type = LibHarvensAddonSettings.ST_BUTTON,
+				label = function()
+					return getLabel(libRadialWheelEntries[i], i)
+				end,
+				buttonText = GetString(SI_GAMEPAD_ITEM_ACTION_QUICKSLOT_ASSIGN), 
+				tooltip = function()
+					return getTooltip(libRadialWheelEntries[i])
+				end,
+				clickHandler = function() ZO_Dialogs_ShowPlatformDialog("LibProfileAssignDialogue", {slotIndex=i}) end,
+			}
+		end
+
+		if LibRadialMenu.isCN then
+			local lookup = {
+				[GetString(SI_LIBRADIALMENU_TRADCN)] = "tradcn",
+				[GetString(SI_LIBRADIALMENU_SIMPLECN)] = "simplecn",
+				["tradcn"] = GetString(SI_LIBRADIALMENU_TRADCN),
+				["simplecn"] = GetString(SI_LIBRADIALMENU_SIMPLECN),
+			}
+
+			table.insert(settingsTable, 1, {
+				type = LibHarvensAddonSettings.ST_DROPDOWN,
+				items = {
+					{name = GetString(SI_LIBRADIALMENU_SIMPLECN), data = "simplecn"},
+					{name = GetString(SI_LIBRADIALMENU_TRADCN), data = "tradcn"},
+				},
+				label = GetString(SI_LIBRADIALMENU_TRANSLATEDBY),
+				getFunction = function()
+					return lookup[LibRadialMenu.vars.cntype] or GetString(SI_LIBRADIALMENU_SIMPLECN)
+				end,
+				setFunction = function(control, itemName, itemData)
+					LibRadialMenu.vars.cntype = itemData.data or "simplecn"
+					LibRadialMenu.UpdateSettingsMenu()
+				end,
+			})
+		elseif GetString(SI_LIBRADIALMENU_TRANSLATEDBY) ~= "" then
+			table.insert(settingsTable, 1, {
+				type = LibHarvensAddonSettings.ST_SECTION,
+				label = GetString(SI_LIBRADIALMENU_TRANSLATEDBY),
+			})
+		end
+
+		settings:AddSettings(settingsTable)
+		if LibHarvensAddonSettings.list then
+			settings:CreateControls()
+		end
+
 	end
 
-	settings:AddSettings(settingsTable)
-	if LibHarvensAddonSettings.list then
-		settings:CreateControls()
-	end
+
 
 end
-
-
-
-
 
 
 
@@ -518,7 +520,9 @@ function LibRadialMenu:Initialize()
 	end
 
 	libRadialWheelEntries = LibRadialMenu.vars.slots
-	LibRadialMenu.UpdateSettingsMenu()
+	if LibHarvensAddonSettings then
+		LibRadialMenu.UpdateSettingsMenu()
+	end
 
 	EVENT_MANAGER:UnregisterForEvent(LibRadialMenu.name, EVENT_ADD_ON_LOADED)
 end
